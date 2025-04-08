bc1ed43379a5047be7c3c4d0168dba36ae9652bc
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-04-08 16:38:09 +0900

    Add err handle and initial obj

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,4 @@
+import { ogpDataInitial } from '@/app/config/initials'
 import { JSDOM } from 'jsdom'
 import { type NextRequest, NextResponse } from 'next/server'
 
@@ -9,39 +10,52 @@ export async function GET(request: NextRequest) {
   const url = searchParams.get('url')
   if (!url) return new Response(null, { status: 400 })
 
-  const dom = await JSDOM.fromURL(url)
-  const document = dom.window.document
+  try {
+    const dom = await JSDOM.fromURL(url)
+    const document = dom.window.document
 
-  const ogpData = {
-    title:
-      document
-        .querySelector('meta[property="og:title"]')
-        ?.getAttribute('content') ||
-      document.querySelector('meta[name="title"]')?.getAttribute('content') ||
-      'No title available',
-    description:
-      document
-        .querySelector('meta[property="og:description"]')
-        ?.getAttribute('content') ||
-      document
-        .querySelector('meta[name="description"]')
-        ?.getAttribute('content') ||
-      'No description available',
-    image:
-      document
-        .querySelector('meta[property="og:image"]')
-        ?.getAttribute('content') ||
-      document.querySelector('meta[name="image"]')?.getAttribute('content') ||
-      document.querySelector('#imgTagWrapperId img')?.getAttribute('src') ||
-      '/default-image.png',
-    favicon:
-      document.querySelector('link[rel="icon"]')?.getAttribute('href') ||
-      document
-        .querySelector('link[rel="shortcut icon"]')
-        ?.getAttribute('href') ||
-      `https://www.google.com/s2/favicons?domain=${new URL(url).host}` ||
-      '/default-favicon.png',
-    urlOriginal: new URL(url).origin,
+    const ogpData = {
+      title:
+        document
+          .querySelector('meta[property="og:title"]')
+          ?.getAttribute('content') ||
+        document.querySelector('meta[name="title"]')?.getAttribute('content') ||
+        'No title available',
+      description:
+        document
+          .querySelector('meta[property="og:description"]')
+          ?.getAttribute('content') ||
+        document
+          .querySelector('meta[name="description"]')
+          ?.getAttribute('content') ||
+        'No description available',
+      image:
+        document
+          .querySelector('meta[property="og:image"]')
+          ?.getAttribute('content') ||
+        document.querySelector('meta[name="image"]')?.getAttribute('content') ||
+        document.querySelector('#imgTagWrapperId img')?.getAttribute('src') ||
+        '/default-image.png',
+      favicon:
+        document.querySelector('link[rel="icon"]')?.getAttribute('href') ||
+        document
+          .querySelector('link[rel="shortcut icon"]')
+          ?.getAttribute('href') ||
+        `https://www.google.com/s2/favicons?domain=${new URL(url).host}` ||
+        '/default-favicon.png',
+      hostname: new URL(url).host,
+      hostLink: `https://${new URL(url).host}`,
+      link: url,
+      error: [],
+    }
+    return NextResponse.json(ogpData)
+  } catch (error) {
+    console.error('Error fetching OGP data:', error)
+    const ogpData = {
+      ...ogpDataInitial,
+      error: ['Failed to fetch OGP data'],
+    }
+    return NextResponse.json(ogpData)
   }
-  return NextResponse.json(ogpData)
+  // const ogpData = {
 }
---
+++ src/app/config/initials.ts
@@ -0,0 +1,19 @@
+export const searchFormInitial = {
+  url: '',
+  error: [] as string[],
+}
+
+export type SearchFormInitial = typeof searchFormInitial
+
+export const ogpDataInitial = {
+  title: '',
+  description: '',
+  image: '',
+  favicon: '',
+  hostname: '',
+  hostLink: '',
+  link: '',
+  error: [] as string[],
+}
+
+export type OGPDataInitial = typeof ogpDataInitial
