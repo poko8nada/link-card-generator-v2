d1d6c6976f7b53f8f2b4365a0786557b5e1b4338
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-04-08 13:50:22 +0900

    add get ogp data logic

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,4 @@
+import { JSDOM } from 'jsdom'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
@@ -8,5 +9,39 @@ export async function GET(request: NextRequest) {
   const url = searchParams.get('url')
   if (!url) return new Response(null, { status: 400 })
 
-  return NextResponse.json(url)
+  const dom = await JSDOM.fromURL(url)
+  const document = dom.window.document
+
+  const ogpData = {
+    title:
+      document
+        .querySelector('meta[property="og:title"]')
+        ?.getAttribute('content') ||
+      document.querySelector('meta[name="title"]')?.getAttribute('content') ||
+      'No title available',
+    description:
+      document
+        .querySelector('meta[property="og:description"]')
+        ?.getAttribute('content') ||
+      document
+        .querySelector('meta[name="description"]')
+        ?.getAttribute('content') ||
+      'No description available',
+    image:
+      document
+        .querySelector('meta[property="og:image"]')
+        ?.getAttribute('content') ||
+      document.querySelector('meta[name="image"]')?.getAttribute('content') ||
+      document.querySelector('#imgTagWrapperId img')?.getAttribute('src') ||
+      '/default-image.png',
+    favicon:
+      document.querySelector('link[rel="icon"]')?.getAttribute('href') ||
+      document
+        .querySelector('link[rel="shortcut icon"]')
+        ?.getAttribute('href') ||
+      `https://www.google.com/s2/favicons?domain=${new URL(url).host}` ||
+      '/default-favicon.png',
+    urlOriginal: new URL(url).origin,
+  }
+  return NextResponse.json(ogpData)
 }
